09b662bcb218b045ee017d074e31651619a64763
mpluisacarolina <mpluisacarolina@gmail.com>
2024-01-25 08:28:06 -0400

    Creación componentes para el layout

---
+++ src/components/layout/HeaderNav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const HeaderNav = () => {
+    return (
+        <header>
+            <h1> Luisa Carolina </h1>
+            <nav>
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
--- src/router/MisRutas.jsx
+++ src/router/MisRutas.jsx
@@ -5,23 +5,27 @@ import { Servicios } from '../components/Servicios';
 import { Portafolio } from '../components/Portafolio';
 import { Curriculum } from '../components/Curriculum';
 import { Contacto } from '../components/Contacto';
+import { HeaderNav } from '../components/layout/HeaderNav';
+import { Footer } from '../components/layout/Footer';
 
 export const MisRutas = () => {
     return (
         <BrowserRouter >
             {/* HEADER Y NAVEGACION */}
+            <HeaderNav />
 
             {/* CONTENIDO CENTRAL */}
             <Routes >
-                <Route path = "/" element = {<Inicio />} />
-                <Route path = "inicio" element = {<Inicio />} />
-                <Route path = "portafolio" element = {<Portafolio />} />
-                <Route path = "servicios" element = {<Servicios />} />
-                <Route path = "curriculum" element = {<Curriculum />} />
-                <Route path = "contacto" element = {<Contacto />} />
+                <Route path="/" element={<Inicio />} />
+                <Route path="inicio" element={<Inicio />} />
+                <Route path="portafolio" element={<Portafolio />} />
+                <Route path="servicios" element={<Servicios />} />
+                <Route path="curriculum" element={<Curriculum />} />
+                <Route path="contacto" element={<Contacto />} />
             </Routes>
-
+            <hr />
             {/* FOOTER */}
+            <Footer />
         </BrowserRouter>
     )
 }
