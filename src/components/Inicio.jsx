222520bfd31094c5cdeb7851e662b58f1814a4f2
mpluisacarolina <mpluisacarolina@gmail.com>
2024-02-07 14:02:11 -0400

    Proyectos Filtrados en Home

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ListadoTrabajos } from './ListadoTrabajos'
 
 export const Inicio = () => {
   return (
@@ -10,7 +11,7 @@ export const Inicio = () => {
         en todo tipo de proyectos web
       </h1>
 
-      <h2>
+      <h2 className='title'>
         Te ayudo a crear tu sitio o aplicación web, tener mas
         visibilidad y relevancia en internet. <Link to="/contacto">Contacta conmigo </Link>
       </h2>
@@ -18,9 +19,8 @@ export const Inicio = () => {
       <section className="last-works">
         <h2 className="heading"> Algunos de mis proyectos </h2>
         <p>Estos son algunos de mis trabajos de desarrollo web</p>
-        <div className="works">
-
-        </div>
+        
+        <ListadoTrabajos limite="2" />
 
       </section>
     </div>
